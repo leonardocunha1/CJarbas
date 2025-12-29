96ad219c7f387cbf6842adc1d400c65cc0efa9ff
Leonardo Cunha <leo.francasp97@gmail.com>
2025-12-28 22:32:26 -0300

    Iniciando estudos

---
+++ Controllers/DeviceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Entities;
+
+namespace MyFirstApi.Controllers;
+
+public class DeviceController : MyFirstApiBaseController
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var laptop = new Laptop();
+        var model = laptop.GetModel();
+
+        return Ok(model);
+    }
+}
---
+++ Entities/Laptop.cs
@@ -0,0 +1,15 @@
+﻿namespace MyFirstApi.Entities;
+
+public class Laptop : Device
+{
+    public string GetModel()
+    {
+        var isConnected = IsConnected();
+        if (isConnected)
+        {
+            return "Laptop Model XYZ";
+        }
+
+        return "Unknow"; 
+    }
+}
