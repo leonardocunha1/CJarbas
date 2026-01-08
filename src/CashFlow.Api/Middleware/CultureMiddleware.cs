ddb89f771f8aae7f362cf8d38b7e824619a0e9dc
Leonardo Cunha <leo.francasp97@gmail.com>
2026-01-08 18:45:40 -0300

    aula rocketseat

---
+++ src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace CashFlow.Api.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task Invoke(HttpContext context)
+    {
+        var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+        // Define a cultura padrão como "en"
+        var cultureInfo = new CultureInfo("en");
+        // Executado quando o header Accept-Language estiver presente e não for vazio ou nulo
+        if (!string.IsNullOrWhiteSpace(culture))
+        {
+            // Tenta definir a cultura com base no header
+            cultureInfo = new CultureInfo(culture);
+        }
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+        await _next(context);
+    }
+}
