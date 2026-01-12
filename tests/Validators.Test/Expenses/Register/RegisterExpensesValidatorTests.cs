390bcb1c69dfc379d8c0aa2659047c5be3a085cf
Leonardo Cunha <leo.francasp97@gmail.com>
2026-01-11 22:46:53 -0300

    aula teste

---
+++ tests/Validators.Test/Expenses/Register/RegisterExpensesValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Test.Expenses.Register;
+
+public class RegisterExpensesValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Description = "Office supplies",
+            Date = DateTime.UtcNow.AddDays(-1),
+            Title = "Purchase of office supplies",
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
+        };
+        // Act
+        var result = validator.Validate(request);
+        // Assert
+        Assert.True(result.IsValid);
+    }
+}
