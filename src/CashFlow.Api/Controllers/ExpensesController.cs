45bc6368eee37487286c70cb9c92ca1d7a744d34
Leonardo Cunha <leo.francasp97@gmail.com>
2026-01-17 17:41:36 -0300

    assincronismo e mapeamento

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,19 +1,22 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[ProducesResponseType(typeof(ResponseRegisteredExpenseJson), StatusCodes.Status201Created)]
+[ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
 public class ExpensesController : ControllerBase
 {
     [HttpPost]
-    public IActionResult Register(
+    public async Task<IActionResult> Register(
         [FromServices] IRegisterExpenseUseCase useCase,
         [FromBody] RequestRegisterExpenseJson request)
     {
-        var response = useCase.Execute(request);
+        var response = await useCase.Execute(request);
         return Created(string.Empty, response);
     }
 }
--- src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
@@ -3,13 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess;
 
-internal class CashFlowDbContext: DbContext
+internal class CashFlowDbContext : DbContext
 {
-    public CashFlowDbContext(DbContextOptions options) : base(options)
+    public CashFlowDbContext(
+        DbContextOptions<CashFlowDbContext> options)
+        : base(options)
     {
-        
     }
-    public DbSet<Expense> Expenses { get; set; }
 
-    
+    public DbSet<Expense> Expenses { get; set; }
 }
