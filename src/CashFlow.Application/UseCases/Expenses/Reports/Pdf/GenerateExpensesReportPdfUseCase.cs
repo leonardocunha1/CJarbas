185a0156dbc49e46154ea3354df168380a610aa3
Leonardo Cunha <leo.francasp97@gmail.com>
2026-01-22 23:05:13 -0300

    Inicio configuracao de pdf para as despesas

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,4 +1,8 @@
-﻿using CashFlow.Domain.Repositories.Expenses;
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Reports;
+using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
+using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 
@@ -10,6 +14,8 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
     public GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository)
     {
         _repository = repository;
+
+        GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
     }
 
     public async Task<byte[]> Execute(DateOnly month)
@@ -20,8 +26,37 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
             return [];
         }
 
-
+        var document = CreateDocument(month);
+        var page = CreatePage(document);
 
         return [];
+    }
+
+    private Document CreateDocument(DateOnly month)
+    {
+        var document = new Document();
+
+        document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSES_FOR} {month:Y}";
+        document.Info.Author = "Leonardo Cunha";
+
+        var style = document.Styles["Normal"];
+        style!.Font.Name = FontHelper.RALEWAY_REGULAR;
+
+        return document;
+    }
+
+    private Section CreatePage(Document document)
+    {
+        var section = document.AddSection();
+        section.PageSetup = document.DefaultPageSetup.Clone();
+
+        section.PageSetup.PageFormat = PageFormat.A4;
+
+        section.PageSetup.LeftMargin = 40;
+        section.PageSetup.RightMargin = 40;
+        section.PageSetup.TopMargin = 80;
+        section.PageSetup.BottomMargin = 80;
+
+        return section;
     }
 }
