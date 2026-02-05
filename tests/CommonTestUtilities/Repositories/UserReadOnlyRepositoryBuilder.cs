20d25b2d8b0752fd305cad8e8ca8930180146bd4
Leonardo Cunha <leo.francasp97@gmail.com>
2026-02-05 18:30:04 -0300

    dia 05-02

--- tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -1,8 +1,7 @@
-﻿using CashFlow.Domain.Repositories.User;
+﻿﻿using CashFlow.Domain.Repositories.User;
 using Moq;
 
 namespace CommonTestUtilities.Repositories;
-
 public class UserReadOnlyRepositoryBuilder
 {
     private readonly Mock<IUserReadOnlyRepository> _repository;
@@ -10,6 +9,11 @@ public class UserReadOnlyRepositoryBuilder
     public UserReadOnlyRepositoryBuilder()
     {
         _repository = new Mock<IUserReadOnlyRepository>();
+    }
+
+    public void ExistActiveUserWithEmail(string email)
+    {
+        _repository.Setup(userReadOnly => userReadOnly.ExistActiveUserWithEmail(email)).ReturnsAsync(true);
     }
 
     public IUserReadOnlyRepository Build() => _repository.Object;
