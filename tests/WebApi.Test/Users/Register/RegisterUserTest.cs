c3486246c60c348e7785303150b920d1f799d9f4
Leonardo Cunha <leo.francasp97@gmail.com>
2026-02-07 22:15:31 -0300

    Teste para Login

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test.Users.Register;
 
@@ -38,10 +39,7 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
     }
 
     [Theory]
-    [InlineData("fr")]
-    [InlineData("en")]
-    [InlineData("pt-BR")]
-    [InlineData("pt-PT")]
+    [ClassData(typeof(CultureInlineDataTest))]
     public async Task Error_Empty_Name(string cultureInfo)
     {
         var request = RequestRegisterUserJsonBuilder.Build();
