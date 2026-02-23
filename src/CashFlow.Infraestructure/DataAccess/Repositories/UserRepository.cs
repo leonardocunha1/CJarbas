175ec05648f67586a298e6120668258eb35b4d66
Leonardo Cunha <leo.francasp97@gmail.com>
2026-02-23 15:26:25 -0300

    Atualizando perfil dos usuários

--- src/CashFlow.Infraestructure/DataAccess/Repositories/UserRepository.cs
+++ src/CashFlow.Infraestructure/DataAccess/Repositories/UserRepository.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess.Repositories;
 
-internal class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepository
+internal class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepository, IUserUpdateOnlyRepository
 {
     private readonly CashFlowDbContext _dbContext;
 
@@ -15,6 +15,11 @@ internal class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepositor
         await _dbContext.Users.AddAsync(user);
     }
 
+    public async Task<User> GetById(long id)
+    {
+        return await _dbContext.Users.FirstAsync(user => user.Id == id);
+    }
+
     public async Task<bool> ExistActiveUserWithEmail(string email)
     {
         return await _dbContext.Users.AnyAsync(user => user.Email.Equals(email));
@@ -23,5 +28,10 @@ internal class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepositor
     public async Task<User?> GetUserByEmail(string email)
     {
         return await _dbContext.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Email.Equals(email));
+    }
+
+    public void Update(User user)
+    {
+        _dbContext.Users.Update(user);
     }
 }
