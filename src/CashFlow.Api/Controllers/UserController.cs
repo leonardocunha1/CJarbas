131da703fc3fde53237d6c9bc783887ce6831d0e
Leonardo Cunha <leo.francasp97@gmail.com>
2026-02-24 14:29:10 -0300

    Adicionando nova migration

--- src/CashFlow.Api/Controllers/UserController.cs
+++ src/CashFlow.Api/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Users.ChangePassword;
+using CashFlow.Application.UseCases.Users.Delete;
 using CashFlow.Application.UseCases.Users.Profile;
 using CashFlow.Application.UseCases.Users.Register;
 using CashFlow.Application.UseCases.Users.Update;
